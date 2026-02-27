ff538168ae75bc5bb30d1ee57eeb3ae20281b43d
Luca Castri <lucacastri94@gmail.com>
2026-02-27 10:04:43 +0100

    new fig

--- utilities_ws/src/RAL/robot_postprocess/scripts/box_plot_reconstruction.py
+++ utilities_ws/src/RAL/robot_postprocess/scripts/box_plot_reconstruction.py
@@ -9,13 +9,15 @@ def plot_boxplot(df, title, ylabel, fontsize=15, outdir = None):
     plt.figure(figsize=(10, 6))
     ax = plt.gca()
 
-    plt.boxplot([df[SCENARIO][k]["data"] for SCENARIO in SCENARIOS for k in df[SCENARIO].keys()], labels=[df[SCENARIO][k]["label"] for SCENARIO in SCENARIOS for k in df[SCENARIO].keys()])
-    plt.title(f"{title}", fontdict={"fontsize": fontsize})
-    plt.ylabel(ylabel, fontdict={"fontsize": fontsize})
+    plt.boxplot([df[SCENARIO][k]["data"] for SCENARIO in SCENARIOS for k in df[SCENARIO].keys()], 
+                tick_labels=[df[SCENARIO][k]["label"] for SCENARIO in SCENARIOS for k in df[SCENARIO].keys()])
+    plt.setp(ax.get_xticklabels(), fontsize=18)     # works too
+    plt.title(f"{title}", fontdict={"fontsize": 18})
+    plt.ylabel(ylabel, fontdict={"fontsize": 18})
     plt.grid(axis='y', linestyle='--', alpha=0.6)
-    plt.xticks(fontsize=fontsize)
-    plt.yticks(fontsize=fontsize)
-    plt.yticks(range(0, ylimit, 1), fontsize=fontsize)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.yticks(range(0, ylimit, 1), fontsize=18)
     
     current_right_lim = len(SCENARIOS)*2 + 0.5
     ax.set_xlim(left=-0.5, right=current_right_lim)
@@ -33,7 +35,7 @@ def plot_boxplot(df, title, ylabel, fontsize=15, outdir = None):
         ax.axhspan(lower, upper, color=color, alpha=0.3)
         # Place text at a fixed position on the far left (e.g., x = -0.4)
         ax.text(-0.4, (lower + upper) / 2, label, 
-                va="center", ha="left", fontsize=fontsize-1, color="black") 
+                va="center", ha="left", fontsize=18, color="black") 
         
     plt.tight_layout()
     if outdir is not None:
@@ -52,7 +54,7 @@ df = {"poster": {"baseline": {"data": [], "label": ""}, "causal": {"data": [], "
 df = {SCENARIO: {"baseline": {"data": [], "label": ""}, "causal": {"data": [], "label": ""}} for SCENARIO in SCENARIOS}
 
 for SCENARIO in SCENARIOS:
-    CSVPATH = "/home/lcastri/git/PeopleFlow/utilities_ws/src/RAL/robot_postprocess/csv/"
+    CSVPATH = "/home/lcastri/git/PeopleFlow/utilities_ws/src/RAL/robot_postprocess/csv-inference/"
     df_baseline = pd.read_csv(CSVPATH + f'tiago_path-{SCENARIO}-baseline.csv')
     df_causal = pd.read_csv(CSVPATH + f'tiago_path-{SCENARIO}-causal.csv')
 
@@ -68,11 +70,11 @@ for SCENARIO in SCENARIOS:
     # 3. Calculate Euclidean Distance for every timestep
     df[SCENARIO]['baseline']["data"] = np.sqrt((df_baseline['x'] - centroid_x)**2 + (df_baseline['y'] - centroid_y)**2)
     df[SCENARIO]['causal']["data"] = np.sqrt((df_causal['x'] - centroid_x)**2 + (df_causal['y'] - centroid_y)**2)
-    df[SCENARIO]['baseline']["label"] = f"baseline-{SCENARIO}"
-    df[SCENARIO]['causal']["label"] = f"causal-{SCENARIO}"
+    df[SCENARIO]['baseline']["label"] = f"baseline-{'S1' if SCENARIO == 'poster' else 'S2'}"
+    df[SCENARIO]['causal']["label"] = f"causal-{'S1' if SCENARIO == 'poster' else 'S2'}"
 
 plot_boxplot(df, 
              title="Human-Robot Proxemic Compliance",
              ylabel="m",
-             outdir=None
+             outdir='/home/lcastri/git/PeopleFlow/utilities_ws/src/RAL/robot_postprocess/result/'
             )
